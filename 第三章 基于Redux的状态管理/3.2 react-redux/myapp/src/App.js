9a9e395f724238a08c3264a9439d00bdae8d66ae
ldf <kfcfk1985@163.com>
2021-07-04 22:20:07 +0800

    优化 react-redux 的例子

--- 第三章 基于Redux的状态管理/3.2 react-redux/myapp/src/App.js
+++ 第三章 基于Redux的状态管理/3.2 react-redux/myapp/src/App.js
@@ -5,18 +5,11 @@ function App(props) {
   const count = useSelector((state) => state.count); //! store.state.count 映射 为 count
   const dispatch = useDispatch(); //! 返回store.dispatch
 
-  console.log("App run"); //! 一旦 dispatch，就会修改 store中的stata，之后这个函数式组件都会运行
+  console.log("App run"); //! 一旦 dispatch，就会修改 store中的stata，之后这个函数式组件都会运行(因为这里建立了映射)
   return (
     <div>
-      <button
-        onClick={() => {
-          dispatch({
-            type: "COUNT_REDUCE",
-          });
-        }}
-      >
-        -
-      </button>
+      {/* //!这种写法比较精简 */}
+      <button onClick={dispatch.bind(null, { type: "COUNT_REDUCE" })}>-</button>
 
       <span>{count}</span>
 
--- 第三章 基于Redux的状态管理/3.2 react-redux/myapp/src/store.js
+++ 第三章 基于Redux的状态管理/3.2 react-redux/myapp/src/store.js
@@ -11,6 +11,7 @@ function countFn(
   switch (action.type) {
     case "COUNT_PLUS":
       return {
+        ...state, //!这种写法比较稳妥
         count: state.count + 1,
       };
     case "COUNT_REDUCE":
