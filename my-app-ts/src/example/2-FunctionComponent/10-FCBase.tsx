3a34ff6f8374e6e1fef3a48f3cedd028c114816d
ldf <kfcfk1985@163.com>
2021-08-01 22:57:43 +0800

    定时器只加一次的原因：虽然setInterval函数里面的函数每秒都会执行一次，但是count值始终是初始的0，因为这个函数绑定了第一轮render之后的count值

--- my-app-ts/src/example/2-FunctionComponent/10-FCBase.tsx
+++ my-app-ts/src/example/2-FunctionComponent/10-FCBase.tsx
@@ -7,8 +7,11 @@ function Counter() {
       console.log("count", count);
       setCount(count + 1);
     }, 1000);
-    //! 定时器只加一次的原因：虽然setInterval函数里面的函数每秒都会执行一次，但是count值始终是初始的0，因为这个函数绑定了第一轮render之后的count值
+    console.log(
+      "//! 定时器只加一次的原因：虽然setInterval函数里面的函数每秒都会执行一次，但是count值始终是初始的0，因为这个函数绑定了第一轮render之后的count值"
+    );
     return () => clearInterval(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <h1>{count}</h1>;
